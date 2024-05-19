08502a5f1317fe95c25a6094cae836a109c5a5e1
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-19 17:07:22 -0300

    Mudança

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,28 @@
-// Hero.tsx
+// Hero.tsx              <Image src={banner1} alt="Texto" width={1200} height={32} />                    <Image src={text} alt="Texto" width={600} height={32} />
+import Image from "next/image";
+import text from '@/assets/text.webp'
+import banner1 from '@/assets/banner1.webp'
 
 export default function Hero() {
     return (
-
+        <div id="home" className="h-screen flex flex-col lg:flex-row items-center justify-center px-8 lg:px-16 lg:pt-0 pt-12">
+            <div className="relative z-10 text-white w-full lg:w-1/3">
+                <div className="text-left mb-4">
+                    <p className="text-sm md:text-xl text-gray-custom font-bold mb-4 pr-0 lg:pr-20">TrueVR é a próxima evolução do mundo de realidade virtual. Nossa visão é ajudar a conectar todas as pessoas do mundo, eliminando distância e tempo. Comece sua aventura agora mesmo, juntos.</p>
+                </div>
+                <div className="flex flex-col lg:flex-row gap-6">
+                    <button className="bg-green-neon text-black text-2xl font-bold py-3 mb-6 lg:mb-0 px-12 rounded swv">
+                        Explorar
+                    </button>
+                    <button className="bg-none flex flex-row items-center text-white text-2xl font-bold py-3 px-10 rounded">
+                        <div className="flex">
+                            Mais
+                        </div>
+                    </button>
+                </div>
+            </div>
+            <div className="w-full lg:w-1/2">
+            </div>
+        </div>
     );
 }
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,41 @@
-// Nav.tsx
+// Nav.tsx                     <Image src={logo} alt="Logo" width={150} height={32} />
+'use client'
+import { useState } from 'react';
+import Image from "next/image";
+import logo from '@/assets/logo.webp'
 
 export default function Nav() {
-    return (
+    const [menuOpen, setMenuOpen] = useState(false);
 
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const scrollToSection = (id: string) => {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+        }
+        if (menuOpen) {
+            setMenuOpen(false);
+        }
+    };
+
+    return (
+        <nav className="bg-transparent lg:bg-background p-8 fixed w-full z-20">
+            <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+                <div className="flex items-center ml-0 lg:ml-8">
+                </div>
+                <div className="md:flex space-x-6 lg:space-x-12 mt-4 md:mt-0">
+                    <button className="text-gray-400 text-base md:text-lg" onClick={() => scrollToSection('home')}>Home</button>
+                    <button className="text-gray-400 text-base md:text-lg" onClick={() => scrollToSection('projeto')}>Projeto</button>
+                    <button className="text-gray-400 text-base md:text-lg" onClick={() => scrollToSection('sobre')}>Sobre</button>
+                    <button className="text-gray-400 text-base md:text-lg" onClick={() => scrollToSection('recursos')}>Recursos</button>
+                </div>
+                <button className="bg-none hidden lg:block border border-green-custom text-green-custom rounded-md text-base md:text-lg font-bold py-2 px-4 mt-4 md:mt-0 mr-0 lg:mr-8">
+                    Sign up
+                </button>
+            </div>
+        </nav>
     );
 }
