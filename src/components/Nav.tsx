ab67bb015319d8bba18987795c800844c16ce579
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-19 19:57:06 -0300

    Mudança

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -25,13 +25,13 @@ export default function Nav() {
         <nav className="bg-transparent lg:bg-background p-8 fixed w-full z-20">
             <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
                 <div className="flex items-center ml-0 lg:ml-8">
-                    <h1 className='text-3xl font-bold text-yellow-500'>WebStore</h1>
+                    <h1 className='text-3xl font-bold text-yellow-custom'>WebStore</h1>
                 </div>
                 <div className="md:flex space-x-6 lg:space-x-12 mt-4 md:mt-0">
-                    <button className="text-yellow-500 text-base md:text-lg" onClick={() => scrollToSection('home')}>Home</button>
-                    <button className="text-white text-base md:text-lg" onClick={() => scrollToSection('projeto')}>Projeto</button>
-                    <button className="text-white text-base md:text-lg" onClick={() => scrollToSection('sobre')}>Sobre</button>
-                    <button className="text-white text-base md:text-lg" onClick={() => scrollToSection('recursos')}>Recursos</button>
+                    <button className="text-yellow-custom text-base md:text-lg" onClick={() => scrollToSection('home')}>Home</button>
+                    <button className="text-white text-base md:text-lg" onClick={() => scrollToSection('product')}>Product</button>
+                    <button className="text-white text-base md:text-lg" onClick={() => scrollToSection('about')}>About</button>
+                    <button className="text-white text-base md:text-lg" onClick={() => scrollToSection('contact')}>Contact</button>
                 </div>
                 <button className="bg-none hidden lg:block text-white rounded-md text-base md:text-lg font-bold py-2 px-4 mt-4 md:mt-0 mr-0 lg:mr-8">
                     Sign up
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        "yellow-custom": "#EFDF56",
+        "gray-custom": "#7E7E7E",
       },
     },
   },
