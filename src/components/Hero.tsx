e0c8685091a1712ec724c6bdf96031ee5b883b68
Victor Bravim <victorrafaelbravim@gmail.com>
2024-05-20 11:48:29 -0300

    Mudança

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,21 +1,40 @@
 // Hero.tsx              <Image src={banner1} alt="Texto" width={1200} height={32} />                    <Image src={text} alt="Texto" width={600} height={32} />
+'use client'
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import banner from '@/assets/banner.png'
 
 export default function Hero() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 768);
+        };
+
+        handleResize();
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+
+    const backgroundImage = isMobile ? "/bg-mobile.png" : "/bg.png";
+
     return (
-        <div id="home" className="h-screen flex flex-col lg:flex-row items-center justify-center px-8 lg:px-48 lg:pt-0 pt-12">
+        <div id="home" className="h-screen flex flex-col lg:flex-row items-center justify-center px-8 lg:px-48 lg:pt-0 pt-12 bg-cover bg-center" style={{ backgroundImage: `url(${backgroundImage})` }}>
             <div className="relative z-10 text-white w-full lg:w-1/2">
                 <div className="text-left mb-4">
-                    <h1 className="text-8xl font-bold text-yellow-custom mb-2">NIKE'S SUMMER</h1>
-                    <h2 className="text-6xl font-bold mb-4">COLLECTIONS 2024</h2>
+                    <h1 className="text-8xl font-bold text-white mb-2">Air Jordan 6 <br /> Retro SP</h1>
                     <p className="text-md text-gray-custom font-bold mb-6 pr-0 lg:pr-36">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque at aliquam risus. Donec at nisl quis augue pretium tempor efficitur eget ex.</p>
                 </div>
                 <div className="flex flex-col lg:flex-row gap-6">
-                    <button className="bg-yellow-custom text-black text-2xl font-bold py-3 mb-6 lg:mb-0 px-12 rounded swv">
+                    <button className="bg-white text-black text-2xl font-bold py-3 mb-6 lg:mb-0 px-12 rounded swv">
                         Explorar
                     </button>
-                    <button className="bg-none flex flex-row items-center text-yellow-custom text-2xl font-bold py-3 px-10 rounded">
+                    <button className="bg-none flex flex-row items-center text-white text-2xl font-bold py-3 px-10 rounded">
                         <div className="flex">
                             Mais
                         </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,6 @@
-// Nav.tsx                     <Image src={logo} alt="Logo" width={150} height={32} />
+// Nav.tsx
 'use client'
 import { useState } from 'react';
-import Image from "next/image";
-import logo from '@/assets/logo.webp'
 
 export default function Nav() {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -25,10 +23,10 @@ export default function Nav() {
         <nav className="bg-transparent lg:bg-background p-8 fixed w-full z-20">
             <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
                 <div className="flex items-center ml-0 lg:ml-8">
-                    <h1 className='text-3xl font-bold text-yellow-custom'>WebStore</h1>
+                    <h1 className='text-3xl font-bold text-white'>WebStore</h1>
                 </div>
                 <div className="md:flex space-x-6 lg:space-x-12 mt-4 md:mt-0">
-                    <button className="text-yellow-custom text-base md:text-lg" onClick={() => scrollToSection('home')}>Home</button>
+                    <button className="text-white text-base md:text-lg" onClick={() => scrollToSection('home')}>Home</button>
                     <button className="text-white text-base md:text-lg" onClick={() => scrollToSection('product')}>Product</button>
                     <button className="text-white text-base md:text-lg" onClick={() => scrollToSection('about')}>About</button>
                     <button className="text-white text-base md:text-lg" onClick={() => scrollToSection('contact')}>Contact</button>
